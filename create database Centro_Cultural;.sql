5f09059677c46f61c3cd985caad6ba8ba47ed1b6
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2023-10-06 15:18:59 -0300

    Update create database Centro_Cultural;.sql

--- create database Centro_Cultural;.sql
+++ create database Centro_Cultural;.sql
@@ -14,22 +14,8 @@ create table Genero(
 Nombre varchar (20) not null primary key,
 Caracteristicas varchar (100) null,
 Origen varchar (200) NULL,
-Instrumento varchar (100) not null,
-Musicos varchar (20) not null,
 Epoca varchar (20) not null,
-Instrumento varchar (20) not null,
-foreign key (Epoca) REFERENCES Epoca (Nombre),
-foreign key (Musicos) REFERENCES Musico (Nombre),
-foreign key (Instrumento) REFERENCES Instrumento(Nombre)
-);
-
-create table Musico(
-Nombre varchar (20) not null primary key,
-F_nacimiento date not null,
-F_muerte date null,
-Historia varchar (200) null,
-Genero varchar (20) not null,
-foreign key (Genero) REFERENCES Genero(Nombre)
+foreign key (Epoca) REFERENCES Epoca (Nombre)
 );
 
 create table Instrumento(
@@ -41,13 +27,6 @@ Tipo varchar (20) not null,
 Material varchar (20) null
 );
 
-create table Musico, Obras Famosas(
-Nombre_Musico varchar (20) not null primary key,
-Nombre_obra varchar (20) not null primary key,
-foreign key (Nombre_Musico) REFERENCES Musico(Nombre),
-foreign key (Nombre_obra) REFERENCES Nombre_obra(Nombre)
-);
-
 create table Obras_Famosas(
 Nombre varchar (20) not null primary key,
 F_creacion int (4) not null,
@@ -56,32 +35,69 @@ Genero varchar (20) not null,
 foreign key (Genero) REFERENCES Genero(Nombre)
 );
 
+create table Musico(
+Nombre varchar (20) not null primary key,
+F_nacimiento date not null,
+F_muerte date null,
+Historia varchar (200) null,
+Genero varchar (20) not null,
+foreign key (Genero) REFERENCES Genero(Nombre)
+);
+
+create table Genero_Instrumento(
+Nombre_Genero varchar (20) not null,
+Nombre_Instrumento varchar (20) not null,
+primary key  (Nombre_Genero, Nombre_Instrumento),
+foreign key (Nombre_Genero) REFERENCES Genero(Nombre),
+foreign key (Nombre_Instrumento) REFERENCES Instrumento(Nombre)
+);
+
+create table Musico_Obras_Famosas(
+Nombre_Musico varchar (20) not null,
+Nombre_Obra varchar (20) not null,
+primary key (Nombre_Musico, Nombre_Obra),
+foreign key (Nombre_Musico) REFERENCES Obras_Famosas(Nombre),
+foreign key (Nombre_Obra) REFERENCES Musico(Nombre)
+);
 
 INSERT INTO Epoca (Nombre, Caracteristicas, A_inicio, A_final, Genero_Epoca)
 VALUES
-    ('Antigua', 'Época histórica marcada por civilizaciones como Egipto y Roma', -3000, 476, 'Clásica'),
-    ('Renacimiento', 'Período de florecimiento cultural y artístico', 1300, 1600, 'Renacimiento'),
-    ('Barroco', 'Época musical caracterizada por la ornamentación y la ópera', 1600, 1750, 'Clásica'),
-    ('Romanticismo', 'Época de expresión emocional en la música y grandes sinfonías', 1820, 1910, 'Romanticismo'),
-    ('Jazz Era', 'Época del jazz con improvisación y ritmos africanos', 1920, 1930, 'Jazz'),
-    ('Rock & Roll', 'Época de revolución musical y el surgimiento del rock', 1950, 1960, 'Rock'),
-    ('Era del Hip-Hop', 'Época del hip-hop y la cultura urbana', 1980, 2000, 'Hip-Hop'),
-    ('Clásica', 'Época de música orquestal y compositores como Mozart y Beethoven', 1750, 1820, 'Clásica'),
-    ('Salsa', 'Época de la salsa y la música latina', 1970, 1980, 'Salsa'),
-    ('Siglo XX', 'Época de experimentación en la música y avances tecnológicos', 1900, 2000, 'Siglo XX');
-    
-INSERT INTO Genero (Nombre, Caracteristicas, Origen, Instrumento, Epoca)
+    ('Antigua', 'Epoca historica marcada por civilizaciones como Egipto y Roma', -3000, 476, 'Clasica'),
+    ('Renacimiento', 'Periodo de florecimiento cultural y artistico', 1300, 1600, 'Renacimiento'),
+    ('Barroco', 'Epoca musical caracterizada por la ornamentacion y la opera', 1600, 1750, 'Clasica'),
+    ('Romanticismo', 'Epoca de expresion emocional en la musica y grandes sinfonias', 1820, 1910, 'Romanticismo'),
+    ('Jazz Era', 'Epoca del jazz con improvisacion y ritmos africanos', 1920, 1930, 'Jazz'),
+    ('Rock & Roll', 'Epoca de revolucion musical y el surgimiento del rock', 1950, 1960, 'Rock'),
+    ('Era del Hip-Hop', 'Epoca del hip-hop y la cultura urbana', 1980, 2000, 'Hip-Hop'),
+    ('Clasica', 'Epoca de musica orquestal y compositores como Mozart y Beethoven', 1750, 1820, 'Clasica'),
+    ('Salsa', 'Epoca de la salsa y la musica latina', 1970, 1980, 'Salsa'),
+    ('Siglo XX', 'Epoca de experimentacion en la musica y avances tecnologicos', 1900, 2000, 'Siglo XX');
+
+INSERT INTO Instrumento (Nombre, U_creacion, Creador, Tipo, Material)
 VALUES
-    ('Rock', 'Genero caracterizado por guitarras electricas y ritmos energicos', 'Estados Unidos', 'Guitarra electrica', 'Rock & Roll'),
-    ('Clasica', 'Musica compuesta para orquestas y ensembles', 'Varios paises', 'Instrumentos orquestales', 'Clasicismo'),
-    ('Jazz', 'Genero musical improvisado con influencias africanas', 'Estados Unidos', 'Saxofon, Trompeta', 'Jazz Era'),
-    ('Hip-Hop', 'Musica basada en el rap y la cultura urbana', 'Estados Unidos', 'Turntables, Microfono', 'Era del Hip-Hop'),
-    ('Pop', 'Musica popular con enfasis en la melodia y el gancho', 'Varios paises', 'Varios instrumentos', 'Siglo XX'),
-    ('Electronica', 'Musica creada con sintetizadores y dispositivos electronicos', 'Varios paises', 'Sintetizador', 'Siglo XX'),
-    ('Reggae', 'Genero jamaicano con un ritmo caracteristico', 'Jamaica', 'Guitarra, Bajo', 'Siglo XX'),
-    ('Salsa', 'Genero de baile latino con influencias caribeñas', 'Varios paises', 'Percusion, Trombon', 'Siglo XX'),
-    ('Country', 'Musica con raices en la musica folklorica estadounidense', 'Estados Unidos', 'Guitarra acustica', 'Siglo XX'),
-    ('R&B', 'Rhythm and Blues con influencias del soul y el jazz', 'Estados Unidos', 'Piano, Voz', 'Siglo XX');
+    ('Piano', 'Año 1700', 'Bartolomeo Cristofori', 'Teclado', 'Madera y metal'),
+    ('Guitarra', 'Siglo XV', 'Anonimo', 'Cuerda', 'Madera y metal'),
+    ('Violin', 'Siglo XVI', 'Andrea Amati', 'Cuerda', 'Madera'),
+    ('Trompeta', 'Siglo XV', 'Anonimo', 'Viento-metal', 'Laton'),
+    ('Bateria', 'Siglo XIX', 'Anonimo', 'Percusion', 'Madera y metal'),
+    ('Flauta', 'Prehistoria', 'Anonimo', 'Viento-madera', 'Madera'),
+    ('Saxofon', 'Siglo XIX', 'Adolphe Sax', 'Viento-metal', 'Laton'),
+    ('organo', 'Siglo III a.C.', 'Ctesibio de Alejandria', 'Teclado', 'Metal y madera'),
+    ('Arpa', 'Siglo XVIII a.C.', 'Anonimo', 'Cuerda', 'Madera'),
+    ('Tambor', 'Prehistoria', 'Anonimo', 'Percusion', 'Piel y madera');    
+
+INSERT INTO Genero (Nombre, Caracteristicas, Origen, Epoca)
+VALUES
+    ('Rock', 'Genero caracterizado por guitarras electricas y ritmos energicos', 'Estados Unidos',  'Rock & Roll'),
+    ('Clasica', 'Musica compuesta para orquestas y ensembles', 'Varios paises', 'Clasica'),
+    ('Jazz', 'Genero musical improvisado con influencias africanas', 'Estados Unidos', 'Jazz Era'),
+    ('Hip-Hop', 'Musica basada en el rap y la cultura urbana', 'Estados Unidos', 'Era del Hip-Hop'),
+    ('Pop', 'Musica popular con enfasis en la melodia y el gancho', 'Varios paises','Siglo XX'),
+    ('Electronica', 'Musica creada con sintetizadores y dispositivos electronicos', 'Varios paises', 'Siglo XX'),
+    ('Reggae', 'Genero jamaicano con un ritmo caracteristico', 'Jamaica', 'Siglo XX'),
+    ('Salsa', 'Genero de baile latino con influencias caribeñas', 'Varios paises', 'Siglo XX'),
+    ('Country', 'Musica con raices en la musica folklorica estadounidense', 'Estados Unidos', 'Siglo XX'),
+    ('R&B', 'Rhythm and Blues con influencias del soul y el jazz', 'Estados Unidos', 'Siglo XX');
 
 INSERT INTO Musico (Nombre, F_nacimiento, F_muerte, Historia, Genero)
 VALUES
@@ -95,19 +111,6 @@ VALUES
     ('Daft Punk', '1993-01-01', '2021-02-22', 'Duo de musica electronica frances conocido por su estilo unico y cascos.', 'Electronica'),
     ('Santana', '1947-07-20', NULL, 'Guitarrista mexicano-estadounidense conocido por su fusion de rock y musica latina.', 'Rock'),
     ('Celia Cruz', '1925-10-21', '2003-07-16', 'La Reina de la Salsa, cantante cubana con una carrera internacional destacada.', 'Salsa');
-
-INSERT INTO Instrumento (Nombre, U_creacion, Creador, Tipo, Material)
-VALUES
-    ('Piano', 'Año 1700', 'Bartolomeo Cristofori', 'Teclado', 'Madera y metal'),
-    ('Guitarra', 'Siglo XV', 'Anonimo', 'Cuerda', 'Madera y metal'),
-    ('Violin', 'Siglo XVI', 'Andrea Amati', 'Cuerda', 'Madera'),
-    ('Trompeta', 'Siglo XV', 'Anonimo', 'Viento-metal', 'Laton'),
-    ('Bateria', 'Siglo XIX', 'Anonimo', 'Percusion', 'Madera y metal'),
-    ('Flauta', 'Prehistoria', 'Anonimo', 'Viento-madera', 'Madera'),
-    ('Saxofon', 'Siglo XIX', 'Adolphe Sax', 'Viento-metal', 'Laton'),
-    ('organo', 'Siglo III a.C.', 'Ctesibio de Alejandria', 'Teclado', 'Metal y madera'),
-    ('Arpa', 'Siglo XVIII a.C.', 'Anonimo', 'Cuerda', 'Madera'),
-    ('Tambor', 'Prehistoria', 'Anonimo', 'Percusion', 'Piel y madera');
 
 INSERT INTO Obras_Famosas (Nombre, F_creacion, Genero)
 VALUES
@@ -123,7 +126,7 @@ VALUES
     ('Celia y Tito', 1981, 'Salsa');
 
 //1
-SELECT G.Nombre AS Genero,G.Caracteristicas AS C_Genero,G.Origen,G.Instrumento, E.Nombre AS Epoca, E.A_inicio AS P_Inicio, M.Nombre AS Nombre_Musico, M.F_nacimiento AS F_Nacimiento_Musico
+SELECT G.Nombre AS Genero,G.Caracteristicas AS C_Genero,G.Origen, E.Nombre AS Epoca, E.A_inicio AS P_Inicio, M.Nombre AS Nombre_Musico, M.F_nacimiento AS F_Nacimiento_Musico
 FROM Genero G
 JOIN Epoca E ON G.Epoca = E.Nombre
 LEFT JOIN Musico M ON G.Nombre = M.Genero;
